46c1d68a9b30fb43e0734b16629788f72ab31539
Aravindh Siva <adamaravindh@gmail.com>
2018-07-02 19:12:40 +0530

    Update mark.js

--- mark.js
+++ mark.js
@@ -1,397 +1,397 @@
 function calc(){
-var one = document.getElementById("pdm").value;
-    
-     if(one == "O" || one == "o")
-	 {
-            var one = 10;
-            var mul1 = one * 4;
-	 }
-   else if(one == "A+" || one == "a+")
-      	 {
-            var one = 9;
-            var mul1 = one * 4;
-	 }
-   else if(one == "A" || one == "a")
-      	 {
-            var one = 8;
-            var mul1 = one * 4;
-	 }
-   else if(one == "B+" || one == "b+")
-      	 {
-            var one = 7;
-            var mul1 = one * 4;
-	 }
-   else if(one == "B" || one == "b")
-      	 {
-            var one = 6;
-            var mul1 = one * 4;
-	 }
-   else if(one == "RA" || one == "ra")
-      	 {
-            var one = 0;
-            var mul1 = one * 4;
-	 }
-   else if(one == "W" || one == "w")
-      	 {
-            var one = 0;
-            var mul1 = one * 4;
-	 }
-   else if(one == "AB" || one == "ab")
-      	 {
-            var one = 0;
-            var mul1 = one * 4;
-	 }
+ var one = document.getElementById("dbms").value;    
+     if(one == "O" || one == "o" || one == 10)
+     {
+             one = 10;
+            var mul1 = one * 3;
+     }
+   else if(one == "A+" || one == "a+" || one == 9)
+         {
+             one = 9;
+             mul1 = one * 3;
+         }
+   else if(one == "A" || one == "a" || one == 8)
+         {
+             one = 8;
+             mul1 = one * 3;
+     }
+   else if(one == "B+" || one == "b+" || one == 7)
+         {
+             one = 7;
+             mul1 = one * 3;
+     }
+   else if(one == "B" || one == "b" ||  one == 6)
+         {
+             one = 6;
+             mul1 = one * 3;
+     }
+   else if(one == "RA" || one == "ra" || one == 0)
+         {
+             one = 0;
+             mul1 = one * 3;
+     }
+   else if(one == "W" || one == "w" || one == 0)
+         {
+             one = 0;
+             mul1 = one * 3;
+     }
+   else if(one == "AB" || one == "ab" || one == 0)
+         {
+             one = 0;
+             mul1 = one * 3;
+     }
   
-    var two = document.getElementById("dbms").value;
+    var two = document.getElementById("pdm").value;
     
-        if(two == "O" || two == "o")
-	 {
-            var two = 10;
-            var mul2 = two * 3;
-	 }
-   else if(two == "A+" || two == "a+")
-      	 {
-            var two = 9;
-            var mul2 = two * 3;
-	 }
-   else if(two == "A" || two == "a")
-      	 {
-            var two = 8;
-            var mul2 = two * 3;
-	 }
-   else if(two == "B+" || two == "b+")
-      	 {
-            var two = 7;
-            var mul2 = two * 3;
-	 }
-   else if(two == "B" || two == "b")
-      	 {
-            var two = 6;
-            var mul2 = two * 3;
-	 }
-   else if(two == "RA" || two == "ra")
-      	 {
-            var two = 0;
-            var mul2 = two * 3;
-	 }
-   else if(two == "W" || two == "w")
-      	 {
-            var two = 0;
-            var mul2 = two * 3;
-	 }
-   else if(two == "AB" || two == "ab")
-      	 {
-            var two = 0;
-           var mul2 = two * 3;
-	 }
-    var three = document.getElementById("java").value;
-     if(three == "O" || three == "o")
-	 {
-            var three = 10;
+        if(two == "O" || two == "o" || two == 10)
+     {
+             two = 10;
+            var mul2 = two * 4;
+     }
+   else if(two == "A+" || two == "a+" || two == 9)
+         {
+             two = 9;
+             mul2 = two * 4;
+     }
+   else if(two == "A" || two == "a" || two == 8)
+         {
+             two = 8;
+             mul2 = two * 4;
+     }
+   else if(two == "B+" || two == "b+" || two == 7)
+         {
+             two = 7;
+             mul2 = two * 4;
+     }
+   else if(two == "B" || two == "b" || two == 6)
+         {
+             two = 6;
+             mul2 = two * 4;
+     }
+   else if(two == "RA" || two == "ra" || two == 0)
+         {
+             two = 0;
+             mul2 = two * 4;
+     }
+   else if(two == "W" || two == "w" || two == 0)
+         {
+             two = 0;
+             mul2 = two * 4;
+     }
+   else if(two == "AB" || two == "ab" || two == 0)
+         {
+             two = 0;
+            mul2 = two * 4;
+     }
+    var three = document.getElementById("jp").value;
+     if(three == "O" || three == "o" || three == 10)
+     {
+             three = 10;
             var mul3 = three * 3;
-	 }
-   else if(three == "A+" || three == "a+")
-      	 {
-            var three = 9;
-            var mul3 = three * 3;
-	 }
-   else if(three == "A" || three == "a")
-      	 {
-            var three = 8;
-            var mul3 = three * 3;
-	 }
-   else if(three == "B+" || three == "b+")
-      	 {
-            var three = 7;
-            var mul3 = three * 3;
-	 }
-   else if(three == "B" || three == "b")
-      	 {
-            var three = 6;
-            var mul3 = three * 3;
-	 }
-   else if(three == "RA" || three == "ra")
-      	 {
-            var three = 0;
-            var mul3 = three * 3;
-	 }
-   else if(three == "W" || three == "w")
-      	 {
-            var three = 0;
-            var mul3 = three * 3;
-	 }
-   else if(three == "AB" || three == "ab")
-      	 {
-            var three = 0;
-            var mul3 = three * 3;
-	 }
+     }
+   else if(three == "A+" || three == "a+" || three == 9)
+         {
+             three = 9;
+             mul3 = three * 3;
+     }
+   else if(three == "A" || three == "a" || three == 8)
+         {
+             three = 8;
+             mul3 = three * 3;
+     }
+   else if(three == "B+" || three == "b+" || three == 7)
+         {
+             three = 7;
+             mul3 = three * 3;
+     }
+   else if(three == "B" || three == "b" || three == 6)
+         {
+             three = 6;
+            mul3 = three * 3;
+     }
+   else if(three == "RA" || three == "ra" || three == 0)
+         {
+             three = 0;
+             mul3 = three * 3;
+     }
+   else if(three == "W" || three == "w" || three == 0)
+         {
+             three = 0;
+             mul3 = three * 3;
+     }
+   else if(three == "AB" || three == "ab" || three == 0)
+         {
+             three = 0;
+             mul3 = three * 3;
+     }
 
 
     var four = document.getElementById("os").value;
     
     
-    if(four == "O" || four == "o")
-	 {
-            var four = 10;
+    if(four == "O" || four == "o" || four == 10)
+     {
+             four = 10;
             var mul4 = four * 3;
-	 }
-   else if(four == "A+" || four == "a+")
-      	 {
-            var four = 9;
-            var mul4 = four * 3;
-	 }
-   else if(four == "A" || four == "a")
-      	 {
-            var four = 8;
-            var mul4 = four * 3;
-	 }
-   else if(four == "B+" || four == "b+")
-      	 {
-            var four = 7;
-            var mul4 = four * 3;
-	 }
-   else if(four == "B" || four == "b")
-      	 {
-            var four = 6;
-            var mul4 = four * 3;
-	 }
-   else if(four == "RA" || four == "ra")
-      	 {
-            var four = 0;
-            var mul4 = four * 3;
-	 }
-   else if(four == "W" || four == "w")
-      	 {
-            var four = 0;
-            var mul4 = four * 3;
-	 }
-   else if(four == "AB" || four == "ab")
-      	 {
-            var four = 0;
-            var mul4 = four * 3;
-	 }
+     }
+   else if(four == "A+" || four == "a+" || four == 9)
+         {
+             four = 9;
+             mul4 = four * 3;
+     }
+   else if(four == "A" || four == "a" || four == 8)
+         {
+             four = 8;
+             mul4 = four * 3;
+     }
+   else if(four == "B+" || four == "b+" || four == 7)
+         {
+             four = 7;
+             mul4 = four * 3;
+     }
+   else if(four == "B" || four == "b" || four == 6)
+         {
+             four = 6;
+             mul4 = four * 3;
+     }
+   else if(four == "RA" || four == "ra" || four == 0)
+         {
+             four = 0;
+             mul4 = four * 3;
+     }
+   else if(four == "W" || four == "w" || four == 0)
+         {
+             four = 0;
+             mul4 = four * 3;
+     }
+   else if(four == "AB" || four == "ab" || four == 0)
+         {
+             four = 0;
+             mul4 = four * 3;
+     }
 
     var five = document.getElementById("toc").value;
     
-        if(five == "O" || five == "o")
-	 {
-            var five = 10;
-            var mul5 = five * 3;
-	 }
-   else if(five == "A+" || five == "a+")
-      	 {
-            var five = 9;
-            var mul5 = five * 3;
-	 }
-   else if(five == "A" || five == "a")
-      	 {
-            var five = 8;
-            var mul5 = five * 3;
-	 }
-   else if(five == "B+" || five == "b+")
-      	 {
-            var five = 7;
-            var mul5 = five * 3;
-	 }
-   else if(five == "B" || five == "b")
-      	 {
-            var five = 6;
-            var mul5 = five * 3;
-	 }
-   else if(five == "RA" || five == "ra")
-      	 {
-            var five = 0;
-            var mul5 = five * 3;
-	 }
-   else if(five == "W" || five == "w")
-      	 {
-            var five = 0;
-            var mul5 = five * 3;
-	 }
-   else if(five == "AB" || five == "ab")
-      	 {
-            var five = 0;
-            var mul5 = five * 3;
-	 }
+        if(five == "O" || five == "o" || five == 10)
+     {
+             five = 10;
+            var mul5 = five * 4;
+     }
+   else if(five == "A+" || five == "a+" || five == 9)
+         {
+             five = 9;
+             mul5 = five * 4;
+     }
+   else if(five == "A" || five == "a" || five == 8)
+         {
+             five = 8;
+             mul5 = five * 4;
+     }
+   else if(five == "B+" || five == "b+" || five == 7)
+         {
+             five = 7;
+             mul5 = five * 4;
+     }
+   else if(five == "B" || five == "b" || five == 6)
+         {
+             five = 6;
+             mul5 = five * 4;
+     }
+   else if(five == "RA" || five == "ra" || five == 0)
+         {             
+             five = 0;
+             mul5 = five * 4;
+     }
+   else if(five == "W" || five == "w" || five == 0)
+         {
+             five = 0;
+             mul5 = five * 4;
+     }
+   else if(five == "AB" || five == "ab" ||  five == 0)
+         {
+             five = 0;
+             mul5 = five * 4;
+     }
 
     var six = document.getElementById("daa").value;
     
         
 
-    if(six == "O" || six == "o")
-	 {
-            var six = 10;
+    if(six == "O" || six == "o" || six == 10)
+     {
+             six = 10;
             var mul6 = six * 3;
-	 }
-   else if(six == "A+" || six == "a+")
-      	 {
-            var six = 9;
-            var mul6 = six * 3;
-	 }
-   else if(six == "A" || six == "a")
-      	 {
-            var six = 8;
-            var mul6 = six * 3;
-	 }
-   else if(six == "B+" || six == "b+")
-      	 {
-            var six = 7;
-            var mul6 = six * 3;
-	 }
-   else if(six == "B" || six == "b")
-      	 {
-            var six = 6;
-            var mul6 = six * 3;
-	 }
-   else if(six == "RA" || six == "ra")
-      	 {
-            var six = 0;
-            var mul6 = six * 3;
-	 }
-   else if(six == "W" || six == "w")
-      	 {
-            var six = 0;
-            var mul6 = six * 3;
-	 }
-   else if(six == "AB" || six == "ab")
-      	 {
-            var six = 0;
-            var mul6 = six * 3;
-	 }
+     }
+   else if(six == "A+" || six == "a+" || six == 9)
+         {
+             six = 9;
+             mul6 = six * 3;
+     }
+   else if(six == "A" || six == "a" ||  six == 8)
+         {
+             six = 8;
+             mul6 = six * 3;
+     }
+   else if(six == "B+" || six == "b+" || six ==7)
+         {
+             six = 7;
+             mul6 = six * 3;
+     }
+   else if(six == "B" || six == "b" ||six == 6)
+         {
+             six = 6;
+             mul6 = six * 3;
+     }
+   else if(six == "RA" || six == "ra" || six == 0)
+         {
+             six = 0;
+             mul6 = six * 3;
+     }
+   else if(six == "W" || six == "w" || six == 0)
+         {
+             six = 0;
+             mul6 = six * 3;
+     }
+   else if(six == "AB" || six == "ab" || six == 0)
+         {
+             six = 0;
+             mul6 = six * 3;
+     }
 
-    var seven = document.getElementById("jpl").value;
+    var seven = document.getElementById("dbmsl").value;
     
     
-    if(seven == "O" || seven == "o")
-	 {
-            var seven = 10;
+    if(seven == "O" || seven == "o" || seven == 10)
+     {
+             seven = 10;
             var mul7 = seven * 2;
-	 }
-   else if(seven == "A+" || seven == "a+")
-      	 {
-            var seven = 9;
-            var mul7 = seven * 2;
-	 }
-   else if(seven == "A" || seven == "a")
-      	 {
-            var seven = 8;
-            var mul7 = seven * 2;
-	 }
-   else if(seven == "B+" || seven == "b+")
-      	 {
-            var seven = 7;
-            var mul7 = seven * 2;
-	 }
-   else if(seven == "B" || seven == "b")
-      	 {
-            var seven = 6;
-            var mul7 = seven * 2;
-	 }
-   else if(seven == "RA" || seven == "ra")
-      	 {
-            var seven = 0;
-            var mul7 = seven * 2;
-	 }
-   else if(seven == "W" || seven == "w")
-      	 {
-            var seven = 0;
-            var mul7 = seven * 2;
-	 }
-   else if(seven == "AB" || seven == "ab")
-      	 {
-            var seven = 0;
-            var mul7 = seven * 2;
-	 }
+     }
+   else if(seven == "A+" || seven == "a+" || seven == 9)
+         {
+             seven = 9;
+             mul7 = seven * 2;
+     }
+   else if(seven == "A" || seven == "a" || seven == 8)
+         {
+             seven = 8;
+             mul7 = seven * 2;
+     }
+   else if(seven == "B+" || seven == "b+" || seven == 7)
+         {
+             seven = 7;
+             mul7 = seven * 2;
+     }
+   else if(seven == "B" || seven == "b" || seven == 6)
+         {
+             seven = 6;
+             mul7 = seven * 2;
+     }
+   else if(seven == "RA" || seven == "ra" || seven == 0)
+         {
+            seven = 0;
+            mul7 = seven * 2;
+     }
+   else if(seven == "W" || seven == "w" || seven == 0)
+         {
+            seven = 0;
+            mul7 = seven * 2;
+     }
+   else if(seven == "AB" || seven == "ab" || seven == 0)
+         {
+            seven = 0;
+            mul7 = seven * 2;
+     }
   
-    var eight = document.getElementById("osl").value;
+    var eight = document.getElementById("jpl").value;
     
         
 
-    if(eight == "O" || eight == "o")
-	 {
-            var eight = 10;
+    if(eight == "O" || eight == "o" || eight == 10)
+     {
+            eight = 10;
             var mul8 = eight * 2;
-	 }
-   else if(eight == "A+" || eight == "a+")
-      	 {
-            var eight = 9;
-            var mul8 = eight * 2;
-	 }
-   else if(eight == "A" || eight == "a")
-      	 {
-            var eight = 8;
-            var mul8 = eight * 2;
-	 }
-   else if(eight == "B+" || eight == "b+")
-      	 {
-            var eight = 7;
-            var mul8 = eight * 2;
-	 }
-   else if(eight == "B" || eight == "b")
-      	 {
-            var eight = 6;
-            var mul8 = eight * 2;
-	 }
-   else if(eight == "RA" || eight == "ra")
-      	 {
-            var eight = 0;
-            var mul8 = eight * 2;
-	 }
-   else if(eight == "W" || eight == "w")
-      	 {
-            var eight = 0;
-            var mul8 = eight * 2;
-	 }
-   else if(eight == "AB" || eight == "ab")
-      	 {
-            var eight = 0;
-            var mul8 = eight * 2;
-	 }
+     }
+   else if(eight == "A+" || eight == "a+" || eight == 9)
+         {
+            eight = 9;
+             mul8 = eight * 2;
+     }
+   else if(eight == "A" || eight == "a" || eight == 8)
+         {
+             eight = 8;
+             mul8 = eight * 2;
+     }
+   else if(eight == "B+" || eight == "b+" || eight == 7)
+         {
+             eight = 7;
+             mul8 = eight * 2;
+     }
+   else if(eight == "B" || eight == "b" || eight == 6)
+         {
+             eight = 6;
+             mul8 = eight * 2;
+     }
+   else if(eight == "RA" || eight == "ra" || eight == 0)
+         {
+             eight = 0;
+             mul8 = eight * 2;
+     }
+   else if(eight == "W" || eight == "w" || eight == 0)
+         {
+             eight = 0;
+             mul8 = eight * 2;
+     }
+   else if(eight == "AB" || eight == "ab" || eight == 0)
+         {
+             eight = 0;
+             mul8 = eight * 2;
+     }
 
-    var nine = document.getElementById("dbl").value;
+    var nine = document.getElementById("osl").value;
     
     
-    if(nine == "O" || nine == "o")
-	 {
-            var nine = 10;
+    if(nine == "O" || nine == "o" || nine == 10)
+     {
+             nine = 10;
             var mul9 = nine * 2;
-	 }
-   else if(nine == "A+" || nine == "a+")
-      	 {
-            var nine = 9;
-            var mul9 = nine * 2;
-	 }
-   else if(nine == "A" || nine == "a")
-      	 {
-            var nine = 8;
-            var mul9 = nine * 2;
-	 }
-   else if(nine == "B+" || nine == "b+")
-      	 {
-            var nine = 7;
-            var mul9 = nine * 2;
-	 }
-   else if(nine == "B" || nine == "b")
-      	 {
-            var nine = 6;
-            var mul9 = nine * 2;
-	 }
-   else if(nine == "RA" || nine == "ra")
-      	 {
-            var nine = 0;
-            var mul9 = nine * 2;
-	 }
-   else if(nine == "W" || nine == "w")
-      	 {
-            var nine = 0;
-            var mul9 = nine * 2;
-	 }
-   else if(nine == "AB" || nine == "ab")
-      	 {
-            var nine = 0;
-            var mul9 = nine * 2;
-	 }
-   var total = mul1 + mul2 + mul3 + mul4 + mul5 + mul6 + mul7 + mul8 + mul9;
-   var div = (total / 25);
+     }
+   else if(nine == "A+" || nine == "a+" || nine == 9)
+         {
+             nine = 9;
+             mul9 = nine * 2;
+     }
+   else if(nine == "A" || nine == "a" || nine == 8)
+         {
+             nine = 8;
+             mul9 = nine * 2;
+     }
+   else if(nine == "B+" || nine == "b+" || nine == 7)
+         {
+             nine = 7;
+             mul9 = nine * 2;
+     }
+   else if(nine == "B" || nine == "b" || nine == 6)
+         {
+             nine = 6;
+             mul9 = nine * 2;
+     }
+   else if(nine == "RA" || nine == "ra" || nine == 0)
+         {
+             nine = 0;
+             mul9 = nine * 2;
+     }
+   else if(nine == "W" || nine == "w" || nine == 0)
+         {
+             nine = 0;
+             mul9 = nine * 2;
+     }
+   else if(nine == "AB" || nine == "ab" || nine == 0)
+         {
+             nine = 0;
+             mul9 = nine * 2;
+     }
+   var total = mul1 + mul2 + mul3 + mul4 + mul5 + mul6 + mul7 + mul8 + mul9;   
+   //alert("Your Total Grade point is:" + total);
+   var div = (total / 26);
    alert("Your GPA is :" + div);
 }
